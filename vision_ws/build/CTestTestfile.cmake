4913a6401539bd43e366496397e92bc94f2529b5
juanjo <juanjoserequenalama@gmail.com>
2021-02-26 13:46:03 +0100

    catkin_make realizado

---
+++ vision_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/servidor/turtlebot-vision/vision_ws/src
+# Build directory: /home/servidor/turtlebot-vision/vision_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
