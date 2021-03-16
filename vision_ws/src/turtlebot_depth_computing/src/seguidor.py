fe33c78e5de3ea80bacf7dc017dde1525a4ddc62
juanjo <juanjoserequenalama@gmail.com>
2021-03-16 12:23:49 +0100

    Ya puedo monitorizar imagenes

--- vision_ws/src/turtlebot_depth_computing/src/seguidor.py
+++ vision_ws/src/turtlebot_depth_computing/src/seguidor.py
@@ -12,26 +12,24 @@ class Seguidor:
 
     def __init__(self):
         self.bridge = CvBridge()
-        cv2.namedWindow("window",1)
+        cv2.namedWindow("vision_robot",cv2.WINDOW_AUTOSIZE)
         self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
 
     def image_callback(self, msg):
-        image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='passthrough')
-        cv2.imshow("window", image)
-        cv2.waitKey()
+        image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+        print(type(image))
+        cv2.imshow("vision_robot", image)
+        cv2.waitKey(3)
 
-rospy.init_node('seguidor')
-seguidor=Seguidor()
-rospy.spin()
 
-# def main():
-#     #Nuestro nodo, seguidor, es iniciado
-#     rospy.init_node('seguidor')
-#     seguidor=Seguidor()
-#     #La siguiente linea sirve unicamente para cuando vaya a finalizar el nodo
-#     rospy.spin()
+def main():
+    #Nuestro nodo, seguidor, es iniciado
+    rospy.init_node('seguidor')
+    seguidor=Seguidor()
+    #La siguiente linea sirve unicamente para cuando vaya a finalizar el nodo
+    rospy.spin()
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 
