ecae74567bcc7f6117493faf71c8148a78121cbb
juanjo <juanjoserequenalama@gmail.com>
2021-05-11 13:38:03 +0200

    fixed indent

--- vision_ws/src/turtlebot_depth_computing/src/imageRepublisher.py
+++ vision_ws/src/turtlebot_depth_computing/src/imageRepublisher.py
@@ -22,8 +22,8 @@ class image_converter:
             print(e)
 
         (rows,cols,channels) = cv_image.shape
-            if cols > 60 and rows > 60 :
-                cv2.circle(cv_image, (50,50), 10, 255)
+        if(cols > 60 and rows > 60) :
+            cv2.circle(cv_image, (50,50), 10, 255)
         
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image,"bgr8"))
