d94bb7611bf7b853bc3c1e7f8856305bbec347f1
juanjo <juanjoserequenalama@gmail.com>
2021-05-11 11:56:07 +0200

    byebye

--- vision_ws/build/turtlebot_depth_computing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/servidor/turtlebot-vision/vision_ws/src/turtlebot_depth_computing
-# Build directory: /home/servidor/turtlebot-vision/vision_ws/build/turtlebot_depth_computing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
