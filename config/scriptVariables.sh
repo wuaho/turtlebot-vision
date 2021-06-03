eb08ef06f97570511c379b3c756637788fc8f86d
Juanjo <56800800+wuaho@users.noreply.github.com>
2021-06-03 20:31:52 +0200

    Update scriptVariables.sh

--- config/scriptVariables.sh
+++ config/scriptVariables.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ### Este es un modelo del script a ser lanzado ya sea en el servidor o en el robot tortuga. Ciertos parametros deberan de ser 
-### reemplazados segun en la maquina en la que se este. Este script debera de ser lanzado mediante . /path_al_script/scriptVariables.sh
+### reemplazados segun en la maquina en la que se este. Este script debera de ser lanzado mediante . /PATH_AL_SCRIPT/scriptVariables.sh
 
 ### Aun debe de ser mejorado ya que tenemos que conseguir que se escriba en el fichero /etc/hosts 
 
 echo export TURTLEBOT_3D_SENSOR=kinect >> ~/.bashrc
 
 #Por defecto esta puesto a http://localhost:11311
-#EN EL SERVIDOR: cambiar localhost por el hostname del robot
+#EN EL SERVIDOR: cambiar localhost por la IP del robot
 #EN EL ROBOT: dejarlo tal y como esta 
 echo export ROS_MASTER_URI=http://localhost:11311 >> ~/.bashrc
 
 #Por defecto, esta variable de entorno no existe
-#EN EL SERVIDOR: introducir la ip del servidor
-#EN EL ROBOT: introducir la ip del robot
+#EN EL SERVIDOR: introducir IP ip del servidor
+#EN EL ROBOT: introducir la IP del robot
 
 echo export ROS_HOSTNAME=IP_DE_ESTA_MAQUINA >> ~/.bashrc
 
